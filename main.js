bba846f11e7e420edef2cb7feba770ac237f794d
jrobinson0529 <jesse0529robinson@gmail.com>
2021-01-16 11:08:09 -0600

    added variables, data, and math

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-console.log("Hello World!");
+
+//****** START VARIABLES ******
+let firstName = 'Beyonce';
+let lastName = 'Knowles';
+
+firstName = 'Jesse';
+lastName = 'Robinson';
+
+const favoriteFood = 'Sushi';
+//****** END VARIABLES ******
+
+// ****** START DATA TYPES ******
+const dog = 'bark';
+// console.log(typeof dog);
+
+const answer = 33;
+const negative = -14;
+const iLoveJavascript = true;
+
+// console.log(typeof answer, typeof iLoveJavascript, typeof negative);
+
+// ****** END DATA TYPES ******
+
+// ****** START MATH ******
+// console.log(Math.pow(3, 2)); // 9
+// console.log(Math.pow(3, 3)); // 9
+
+// console.log(Math.round(6.45)); // 6
+// console.log(Math.floor(1.2131322)); // 1
+// console.log(Math.ceil(6.0000000001)) // 7
+
+// console.log(Math.floor(Math.random()* 10)+ 1);
+const cat = 'kitty';
+// console.log(cat.length); // 5
+
+// ****** END MATH ******
