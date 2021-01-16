d8c458c4f9e53b97e90ca956b5480408f91431cb
jrobinson0529 <jesse0529robinson@gmail.com>
2021-01-16 13:09:48 -0600

    Went through conditional logic

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+// -------- CONTROL FLOW --------
+
+// function thisIsTrue(arg1, arg2) {
+//   if (arg1 !== 'nice' && arg2 !== 'Nice') {
+//     console.log(`You are ${arg1}.`);
+//   } else {
+//     console.log('false');
+//   }
+// }
+// thisIsTrue('nice', 'Nice', 'not nice'); // JS Does not care about extra arguments.
+// function thisIsTrue(arg1, arg2) {
+//   if (arg1.length < 3) {
+//     console.log(`You are ${arg1}.`);
+//   } else if (arg2.length > 8){
+//     console.log(`You are ${arg2}`);
+//   } else {
+//     console.log('You are NOTHING');
+//   }
+// }
+// thisIsTrue('nice', 'mickeymouse');
+
+const canDrive = (age) => {
+  if (age >= 15) {
+    return true;
+  } 
+
+  return false;
+};
+const answer = canDrive(13);
+console.log(answer);
