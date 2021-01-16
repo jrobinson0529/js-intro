f2f2ed07894506d7121bd5a64f02b7879b266311
jrobinson0529 <jesse0529robinson@gmail.com>
2021-01-16 12:20:41 -0600

    Added functions types practice, functions with arguments

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-console.log("Hello World!");
+// ******* START FUNCTIONS *******
+// --------- Function Types ---------
+// oneMore(); // Hoisted function call
+
+function yetAnother() {
+  console.log('Do something');
+};
+const anotherOne = function() {
+  console.log('Do it');
+};
+function oneMore() {
+  console.log('Hoisted function call!');
+};
+//Calling a function
+// yetAnother();
+
+const sayHello = () => {
+  console.log('What up world');
+};
+
+// sayHello();
+
+// --------- PASSING ARGUMENTS TO A FUNCTION ---------
+// function withArg(argument){
+//   console.log("My argument was: " + argument);
+// };
+// CALLED -
+// withArg("Hello World!");
+
+// ----------- FUNCTIONS WITH ARGUMENTS -----------
+
+function logsHello(name) {
+  console.log(`Hello, ${name}!`);   // Why does this function need no ;
+}
+
+// logsHello('Jesse Robinson');
+
+const addsTwoNumbers = (num1, num2) => {
+  const sum = num1 + num2;
+  console.log(sum);
+  return sum;
+}
+console.log(addsTwoNumbers(3, 5));
+// -------- CONTROL FLOW & COMPARISON OPERATORS -------- Next
